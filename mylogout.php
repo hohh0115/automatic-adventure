9d18aee92757efffb02d1e94aa2b3e21cda91fc5
hohh0115 <zd29430@gmail.com>
2016-12-27 20:06:20 +0800

    會員登出功能

--- mylogout.php
+++ mylogout.php
@@ -3,13 +3,11 @@
 
  $link = $_SESSION['position']; 
  $ifincar =explode("/", $link);//如果在cart.php登出，請跳轉至首頁
-
  if (!isset($_SESSION['user'])) {
   header('Location: index.php'); 
  } else if(isset($_SESSION['user'])!="") {
   header('Location: index.php');
  }
-
  if (isset($_GET['logout'])) {
   unset($_SESSION['user']);
   //session_destroy(); 不用刪掉所有的session 所以註解掉 我要保留購物車的商品
